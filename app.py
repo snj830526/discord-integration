719644ec544beadcc45dcc0bad747f5221600a5a
john choi <john@johns-MacBook-Pro.local>
2024-04-15 13:35:36 +0900

    코드 정리

--- app.py
+++ app.py
@@ -1,54 +1,49 @@
-import discord
 import os
-import time
-
 from datetime import datetime
-from other_app import OtherApp
 
+import discord
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
+from other_app import OtherApp
 
-TOKEN=os.environ['DISCORD_TOKEN']
-CHANNEL_ID=os.environ['DISCORD_CHANNEL_ID']
-other=OtherApp()
+TOKEN = os.environ['DISCORD_TOKEN']
+CHANNEL_ID = os.environ['DISCORD_CHANNEL_ID']
+other = OtherApp()
 
 
 class DiscordClient(discord.Client):
-    
-    
+
     async def start_working(self):
         channel = self.get_channel(int(CHANNEL_ID))
         message = f'[현재시간] {self.get_date()} {self.get_time()}'
         await channel.send(message)
-        
-    
+
     async def on_ready(self):
         scheduler = AsyncIOScheduler()
         scheduler.add_job(self.start_working, CronTrigger(hour="*", minute="0", second="0"))
         scheduler.start()
-        
-        
+
     async def on_message(self, message):
         received = message.content
-        
+
         try:
             if received.lower() == 'ping':
                 await message.channel.send('pong {0.author.mention}'.format(message))
-                
+
             if 'sell' in received.lower():
                 msg = f'[매도] your order : {received.strip("sell")}'
                 msg += ' {0.author.mention}'.format(message)
                 print(msg)
                 await message.channel.send(msg)
-                
+
             if 'refresh' in received.lower():
                 codes = other.refresh_market_codes()
                 msg = f'[갱신] market codes : {len(codes)} 건'
                 msg += ' {0.author.mention}'.format(message)
                 print(msg)
                 await message.channel.send(msg)
-                
+
             if 'show' in received.lower():
                 code = received.lower().strip("show").strip()
                 codes = other.get_market_codes(code)
@@ -56,27 +51,25 @@ class DiscordClient(discord.Client):
                 msg += ' {0.author.mention}'.format(message)
                 print(msg)
                 await message.channel.send(msg)
-                
+
             if 'buy' in received.lower():
                 msg = f'[매수] your order : {received.strip("buy")}'
                 msg += ' {0.author.mention}'.format(message)
                 print(msg)
                 await message.channel.send(msg)
-        
+
         except Exception as e:
             print(f'error: {e}')
-            
-           
+
     def get_time(self):
         return datetime.today().strftime('%H:%M:%S')
-    
-    
+
     def get_date(self):
         return datetime.today().strftime('%Y년 %m월 %d일')
-            
+
 
 intents = discord.Intents.default()
-intents.message_content=True
+intents.message_content = True
 
 client = DiscordClient(intents=intents)
 
--- other_app.py
+++ other_app.py
@@ -2,26 +2,23 @@ import os
 import requests
 import json
 
-
 code_file = 'market_codes.json'
 api_url = 'https://api.upbit.com'
 
 
 class OtherApp():
-    
-    
+
     def __init__(self):
         pass
-    
-    
+
     def get_market_codes(self, code=None):
         saved = self.read_codes()
-        if saved == None or len(saved) == 0:
+        if saved is None or len(saved) == 0:
             url = f"{api_url}/v1/market/all?isDetails=true"
             headers = {"accept": "application/json"}
             res = requests.get(url, headers=headers)
             saved = self.save_codes(res.json())
-        
+
         result = []
         if code is not None and code != '':
             for s in saved:
@@ -30,30 +27,26 @@ class OtherApp():
                     result.append(f'{s["market"]} / {s["korean_name"]}')
         else:
             result = saved
-        
+
         return result
-        
-        
+
     def refresh_market_codes(self):
         if os.path.isfile(code_file):
             os.remove(code_file)
-            
+
         return self.get_market_codes()
-    
-    
+
     def save_codes(self, codes):
         print('writing codes')
         with open(code_file, 'w') as json_file:
             json.dump(codes, json_file)
-        
-        return codes
-        
 
+        return codes
 
     def read_codes(self):
-        if os.path.isfile(code_file):    
+        if os.path.isfile(code_file):
             with open(code_file, 'r') as json_file:
-                if json_file != None:
+                if json_file is not None:
                     codes = json.load(json_file)
                     return codes
                 else:
